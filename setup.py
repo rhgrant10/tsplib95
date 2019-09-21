d16147ea1d597ff9693e1140aaf34b20b9a28d1f
Robert Grant <rhgrant10@gmail.com>
2019-09-21 13:57:56 -0700

    Add offical 3.7 support

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
         'Operating System :: OS Independent',
     ],
