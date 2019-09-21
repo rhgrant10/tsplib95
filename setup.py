2fed8af272528e0894ac92687d01a9efc3b4e8af
Robert Grant <rhgrant10@gmail.com>
2019-09-21 14:01:58 -0700

    Bump version: 0.3.3 → 0.4.0

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/rhgrant10/tsplib95',
-    version='0.3.3',
+    version='0.4.0',
     zip_safe=True,
 )
