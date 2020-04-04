f957b6e87521cbb30cb74bcc8b8579d332774d79
Robert Grant <rhgrant10@gmail.com>
2020-04-04 14:00:18 -0700

    Add FileMeta type tests

---
+++ tests/models/test_filemeta.py
@@ -0,0 +1,71 @@
+import pytest
+
+from tsplib95 import models as M
+from tsplib95 import fields as F
+
+
+QUUX_VALUE = object()
+FOO_FIELD = F.Field('FOO')
+BAZ_FIELD = F.Field('BAZ')
+QUUX_FIELD = F.Field('QUUX')
+
+
+class BaseProblem(metaclass=M.FileMeta):
+    foo = FOO_FIELD
+    bar = 42
+    quux = QUUX_FIELD
+
+
+class CustomProblem(BaseProblem):
+    baz = BAZ_FIELD
+    qux = 3.14
+    quux = QUUX_VALUE
+
+
+@pytest.mark.parametrize('Problem,correct', [
+    (BaseProblem, {'foo': FOO_FIELD, 'quux': QUUX_FIELD}),
+    (CustomProblem, {'foo': FOO_FIELD, 'baz': BAZ_FIELD, 'quux': QUUX_FIELD}),
+])
+def test_fields_by_name(Problem, correct):
+    assert Problem.fields_by_name == correct
+
+
+@pytest.mark.parametrize('Problem,correct', [
+    (BaseProblem, {'FOO': FOO_FIELD, 'QUUX': QUUX_FIELD}),
+    (CustomProblem, {'FOO': FOO_FIELD, 'BAZ': BAZ_FIELD, 'QUUX': QUUX_FIELD}),
+])
+def test_fields_by_keyword(Problem, correct):
+    assert Problem.fields_by_keyword == correct
+
+
+@pytest.mark.parametrize('Problem,correct', [
+    (BaseProblem, {'FOO': 'foo', 'QUUX': 'quux'}),
+    (CustomProblem, {'FOO': 'foo', 'BAZ': 'baz', 'QUUX': 'quux'}),
+])
+def test_names_by_keyword(Problem, correct):
+    assert Problem.names_by_keyword == correct
+
+
+@pytest.mark.parametrize('Problem,correct', [
+    (BaseProblem, {'foo': 'FOO', 'quux': 'QUUX'}),
+    (CustomProblem, {'foo': 'FOO', 'baz': 'BAZ', 'quux': 'QUUX'}),
+])
+def test_keywords_by_name(Problem, correct):
+    assert Problem.keywords_by_name == correct
+
+
+@pytest.mark.parametrize('Problem,correct', [
+    (BaseProblem, {'bar': 42}),
+    (CustomProblem, {'bar': 42, 'qux': 3.14, 'quux': QUUX_VALUE}),
+])
+def test_non_fields_are_class_attributes(Problem, correct):
+    assert all(getattr(Problem, k) == v for k, v in correct.items())
+
+
+@pytest.mark.parametrize('Problem', [
+    BaseProblem,
+    CustomProblem,
+])
+def test_fields_are_not_class_attributes(Problem):
+    with pytest.raises(AttributeError):
+        Problem.foo
